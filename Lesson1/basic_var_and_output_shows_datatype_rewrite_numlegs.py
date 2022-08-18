781ddd036a2a9387e096331ecfb52827d165f691
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-08-18 12:14:53 +1200

    Add files via upload

---
+++ Lesson1/basic_var_and_output_shows_datatype_rewrite_numlegs.py
@@ -0,0 +1,12 @@
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+print (type(name))
+print (type(numLegs))
+print (type(height))
+print (type(hasTail))
+
+numLegs = 3
+print (numLegs)
---
+++ Lesson1/basic_var_and_output_with_labels.py
@@ -0,0 +1,9 @@
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+print ('The animal:',name)
+print ('This number of legs:',numLegs)
+print ('Height in meters:',height)
+print ('It has a tail:',hasTail)
