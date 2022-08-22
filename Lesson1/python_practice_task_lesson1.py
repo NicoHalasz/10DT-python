cc6db3385ea18d77297a4357c4ef3e2e2909da61
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-08-23 09:45:16 +1200

    Add files via upload

---
+++ Lesson1/python_practice_task_lesson1.py
@@ -0,0 +1,17 @@
+# N L Halasz
+# Practice Task
+
+# Variable declaration
+firstName = "Lightning"
+lastName = "Mcqueen" 
+height = 2
+adress = "102 Radiator Springs"
+number = "08008383838003"
+hasAllergy = False
+
+#  Display info
+print ('My name is', firstName, lastName)
+print ('I am', height, 'meters tall')
+print ('I live at', adress)
+print ('My phone number is', number)
+print ('It is', hasAllergy,'that I have an allergy')
