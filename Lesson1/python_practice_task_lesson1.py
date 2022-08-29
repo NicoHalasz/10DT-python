b0a7a50354d909aac606cad4957f90dd7f7f9d50
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-08-30 09:01:24 +1200

    Update python_practice_task_lesson1.py

--- Lesson1/python_practice_task_lesson1.py
+++ Lesson1/python_practice_task_lesson1.py
@@ -1,5 +1,5 @@
 # N L Halasz
-# Practice Task
+# Variables, output, labels, data tpes
 
 # Variable declaration
 firstName = "Lightning"
@@ -8,9 +8,28 @@ height = 2
 adress = "102 Radiator Springs"
 number = "08008383838003"
 hasAllergy = False
+title = "Sir"
 
-#  Display info
-print ('My name is', firstName, lastName)
+# Summary of the information
+print(firstName)
+print(lastName)
+print(height)
+print(adress)
+print(number)
+print(hasAllergy)
+print(title)
+
+# Clarifying the problem
+
+    # Full name including title
+    # Height is in metres
+    # Adress stored in street number and street
+    # Country is not reqiured
+    # No detail of allergy type is required
+    # Data is to be displayed in context of a readable language - labels`
+
+#  Display information with labels
+print ('My name is',title, firstName, lastName)
 print ('I am', height, 'meters tall')
 print ('I live at', adress)
 print ('My phone number is', number)
