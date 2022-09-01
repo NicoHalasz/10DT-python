09346088fa65b71f89950d51bc444d888e56aba2
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-09-01 12:08:05 +1200

    Update input_user.py

--- Lesson1/input_user.py
+++ Lesson1/input_user.py
@@ -11,3 +11,15 @@
 
 # name = input("Enter your name: ")
 # print(name)
+
+# Taking input from the user as integer
+# num = int(input("Enter a number: "))
+# add = num + 1
+# Output
+# print(add)
+
+# Taking input from a user as a float
+num = float(input("Enter a number: "))
+add = num + 2
+# Output
+print(add)
