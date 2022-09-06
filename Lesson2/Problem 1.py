5a2026e2010edc6003f8c777b111a951ee9a0da8
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-09-07 10:45:38 +1200

    Create Problem 1.py

---
+++ Lesson2/Problem 1.py
@@ -0,0 +1,9 @@
+# N L Halasz
+# Problem 01
+# This program is designed to track and total goals scored by the RC first XI for each game played.
+
+match1_ST_Pats = 7
+match2_Well_College = 5
+match3_Well_High = 10
+Team_Total = match1_ST_Pats + match2_Well_College + match3_Well_High
+print ("Total Goals Scored:",Team_Total)
