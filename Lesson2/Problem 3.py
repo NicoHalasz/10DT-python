49b8f32883b2734d501d522b28cfbb816844077b
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-09-08 11:46:37 +1200

    Update Problem 3.py

--- Lesson2/Problem 3.py
+++ Lesson2/Problem 3.py
@@ -12,4 +12,8 @@ print("95 price $" + str(petrol95Price))
 print("91 unleaded price $" + str(petrol91Price))
 dieselPrice = dieselPrice * 1.1
 petrol95Price = petrol95Price * 1.1
-petrol91Price
+petrol91Price = petrol91Price * 1.1
+print("Price increase of 10%:")
+print("Diesel price $" + str(dieselPrice))
+print("95 price $" + str(petrol95Price))
+print("91 unleaded price $" + str(petrol91Price))
