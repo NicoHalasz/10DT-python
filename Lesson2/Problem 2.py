8a06db6bfb5f8d5358388f34d2240589ff65878b
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-09-07 14:35:53 +1200

    Code and stuff

---
+++ Lesson2/Problem 2.py
@@ -0,0 +1,13 @@
+# Nico Halasz
+# Representing number data types as string
+
+# Problem 02
+# This program is desinged to track the value remaining on a gift card as it is used.
+
+Card_Balance = 50.00
+Purchase_01 = 35.00
+print ("Your Gift Card Currently Has $" + str (Card_Balance))
+
+Card_Balance = Card_Balance - Purchase_01
+print ("This purchase cost $" + str (Purchase_01))
+print ("You gift card now has $" + str(Card_Balance))
