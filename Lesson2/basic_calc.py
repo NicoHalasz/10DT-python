95d7e6f473ab3654c55cabfbd747e92756d103c4
NicoHalasz <103463243+NicoHalasz@users.noreply.github.com>
2022-09-13 09:40:51 +1200

    basic_calc added invalid entry alert

--- Lesson2/basic_calc.py
+++ Lesson2/basic_calc.py
@@ -35,3 +35,6 @@ elif operation == "4":
     num1 = input("Enter first number: ")
     num2 = input("Enter second number: ")
     print("The sum is " + str(int(num1) / int(num2)) + ".")
+
+else:
+    print("Invalid entry")
